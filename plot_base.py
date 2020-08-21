d295625179cdb5716b5c840bf753a472f91503bb
chrisshen <shenyiwen7@gmail.com>
2020-08-22 02:47:02 +0900

    plot line cdf

--- plot_base.py
+++ plot_base.py
@@ -13,12 +13,12 @@ from matplotlib.pyplot import figure
 class PlotBase():
 	def drawLine(self, x, y, errbar, labelText):
 		# ax = plt.axes()
-		fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+		fig, ax = plt.subplots(1, 1, figsize=(7, 4))
 		ax.errorbar(x, y, yerr=errbar, capsize=8, label=labelText,
 		            linewidth=3, marker='o', markersize=10)
 		ax.set_xticks(ticks=x)
-		ax.set_xlabel('Sigma (Gaussian Noise)')
-		ax.set_ylabel('Localization Error (m)')
+		ax.set_xlabel('Sigma (Gaussian Noise)', size=15)
+		ax.set_ylabel('Localization Error (m)', size=15)
 		# ax.set_title('')
 		ax.legend(loc='best')
 		
@@ -30,11 +30,12 @@ class PlotBase():
 		pass
 
 	def drawCDF(self, x, y, labelText):
-		fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+		fig, ax = plt.subplots(1, 1, figsize=(7, 4))
 		ax.plot(x, y, label=labelText, linewidth=3)
+		ax.set_xlim(0.0, 5.7)
 		ax.set_ylim(0.0, 1.01)
-		ax.set_xlabel('Localization Error (m)')
-		ax.set_ylabel('CDF')
+		ax.set_xlabel('Localization Error (m)', size=15)
+		ax.set_ylabel('CDF', size=15)
 		
 		ax.legend(loc='best', fontsize=15)
 		fig.tight_layout()
