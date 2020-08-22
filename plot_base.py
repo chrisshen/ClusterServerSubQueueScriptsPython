124820a9534b3ba538ee8dfa781de56e452ddad4
chrisshen <shenyiwen7@gmail.com>
2020-08-22 23:41:24 +0900

    multi CDF

--- plot_base.py
+++ plot_base.py
@@ -14,8 +14,8 @@ class PlotBase():
 	def drawLine(self, x, y, errbar, labelText):
 		# ax = plt.axes()
 		fig, ax = plt.subplots(1, 1, figsize=(7, 4))
-		ax.errorbar(x, y, yerr=errbar, capsize=8, label=labelText,
-		            linewidth=3, marker='o', markersize=10)
+		ax.errorbar(x, y, yerr=errbar, color='blue', capsize=8, label=labelText,
+		            linewidth=3, marker='o', markersize=8)
 		ax.set_xticks(ticks=x)
 		ax.set_xlabel('Sigma (Gaussian Noise)', size=15)
 		ax.set_ylabel('Localization Error (m)', size=15)
@@ -41,7 +41,33 @@ class PlotBase():
 		fig.tight_layout()
 		plt.show()
 		fig.savefig(f'/home/chris/test-d-{labelText}.eps')
-	
+
+	def drawMultiCDF(self, x, y, labelText):
+		fig, ax = plt.subplots(1, 1, figsize=(7, 4))
+
+		ax.axhline(0.8, c='black', ls='--', lw=2)
+		x80 = []
+		for ind in range(len(x)):
+			ax.plot(x[ind], y[ind], label=labelText[ind], linewidth=3)
+			for ind2 in range(len(x[ind])):
+				if round(y[ind][ind2], 2) == 0.80:
+					print(round(y[ind][ind2], 2))
+					x80.append(x[ind][ind2])
+					break
+		print(x80)
+		for ele in x80:
+			ax.axvline(ele, ymax=0.79, c='black', ls='--', lw=2)
+
+		ax.set_xlim(0.0, x[-1][-1])
+		ax.set_ylim(0.0, 1.01)
+		ax.set_xlabel('Localization Error (m)', size=15)
+		ax.set_ylabel('CDF', size=15)
+
+		ax.legend(loc='best', fontsize=15)
+		fig.tight_layout()
+		plt.show()
+		fig.savefig(f'/home/chris/test-d-{labelText}.eps')
+
 	def extractData(self, filename):
 		x = []
 		y = []
@@ -70,22 +96,48 @@ class PlotBase():
 				# print(x, y, errbar)
 		return x, y
 
+	def extractMultiCDFData(self, folder):
+		x = []
+		y = []
+		for root, dirs, files in os.walk(folder):
+			for file in files:
+				with open(root+file, 'r') as f:
+					# data=f.readlines()
+					data = f.read().splitlines()
+					xEle = []
+					yEle = []
+					for ele in data:
+						eleList = ele.split(' ')
+						xEle.append(float(eleList[0]))
+						yEle.append(float(eleList[1]))
+					x.append(xEle)
+					y.append(yEle)
+						# print(x, y, errbar)
+		return x, y
+
 def parseArgments():
 	parser = argparse.ArgumentParser(description='Specify data to draw.')
-	parser.add_argument('--dataFile', type=str, nargs=1, help='data file to draw')
-	parser.add_argument('--labelText', type=str, nargs=1, help='label for the data shown in legend')
+	parser.add_argument('--dataFile', type=str, nargs=1, default=[''], 
+						help='data file to draw')
+	parser.add_argument('--dataFolder', type=str, nargs=1,
+	                    default=[''], help='data folder to collect and draw')
+	parser.add_argument('--labelText', type=str, nargs='*',
+	                    default=[''], help='label for the data shown in legend')
 	return parser
 
 if __name__ == "__main__":
 	parser = parseArgments()
 	para = parser.parse_args()
 	filename = para.dataFile[0]
-	labelText = para.labelText[0]
-
+	labelText = para.labelText
+	folder = para.dataFolder[0]
+	print(labelText)
 	draw = PlotBase()
 	# x,y,errbar=draw.extractData(filename)
-	x, y = draw.extractCDFData(filename)
-
+	# x, y = draw.extractCDFData(filename)
+	x, y = draw.extractMultiCDFData(folder)
+	# print(x[1], y[1])
+	# sys.exit()
 	# draw.drawLine(x, y, errbar, labelText)
-	draw.drawCDF(x, y, labelText)
+	draw.drawMultiCDF(x, y, labelText)
 	# print(x, y, errbar)
